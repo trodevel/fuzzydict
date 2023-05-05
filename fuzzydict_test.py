fd2b7be1f6333c3b70594b45773be80f16f22cd8
trodevel <trodevel@gmail.com>
2023-05-05 03:11:53 +0200

    intermediate results

--- fuzzydict_test.py
+++ fuzzydict_test.py
@@ -20,8 +20,8 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 '''
 
-from fuzzydict.fuzzydict import fuzzydict
-from fuzzydict.fuzzydict import fuzzydict_loader
+import fuzzydict
+import fuzzydict_loader
 
 ##########################################################
 
